3359af180b2a2895bdf315699dad5215fdf94049
Jvntrials <jovanyosa10@gmail.com>
2025-08-19 11:25:57 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,8 +2,8 @@ export const CONSTANT_INVENTORY: { item: string, costPerUnit: number }[] = [
   { item: "Quickmelt Cheese", costPerUnit: 725 },
   { item: "Spaghetti Sauce", costPerUnit: 94 },
   { item: "6 inch square Dough", costPerUnit: 9 },
-  { item: "Ham", costPerUnit: 70 },
-  { item: "Pepperoni", costPerUnit: 80 },
-  { item: "Bacon", costPerUnit: 90 },
-  { item: "Mantou Breadsticks", costPerUnit: 25 },
+  { item: "Ham", costPerUnit: 55 },
+  { item: "Pepperoni", costPerUnit: 380 },
+  { item: "Bacon", costPerUnit: 360 },
+  { item: "Mantou Breadsticks", costPerUnit: 18 },
 ];
